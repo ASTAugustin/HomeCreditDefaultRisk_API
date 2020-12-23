248e4698ad101b5647be6dbe2dbdb20c96cbcc03
ASTAugustin <ASTAugustin@163.com>
2020-12-23 14:17:58 +0800

    Sum Up

--- API/flask/example.py
+++ API/flask/example.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import numpy as np
+import joblib
+import pandas as pd 
 
 app=Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return "Hello World"
+
+@app.route("/", methods=['POST'])
+def call():
+  payload = request.json 
+  x = (type(payload).__name__ == 'dict')
+  print(payload)
+  return jsonify({'predict': 0})
 if __name__=='__main__':
-  app.run(host="0.0.0.0", port=80)
+  app.run(host="0.0.0.0", port=8080)
